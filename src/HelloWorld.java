ab4cec0f163a48e0ec6311263aee6aa05f22dda6
ishikawanagito <ishikawanagito@localhost>
2025-05-12 15:01:40 +0900

    コミット

---
+++ src/HelloWorld.java
@@ -0,0 +1,32 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		String name = "たろう";
+		System.out.println(name);
+		
+		int x = 4;
+		int y = 6;
+		int z = x*y;
+		System.out.println(z);
+		
+		boolean a = false;
+		System.out.println(a);
+		
+		char b = 'A';
+		System.out.println(b);
+		
+		float c = 6.28f;
+		System.out.println(c);
+		
+		long d = 123456789012345L;
+		System.out.println(d);
+		
+		String e = "プログラミングは楽しい！";
+		System.out.println(e);
+		
+	
+	}
+
+}
