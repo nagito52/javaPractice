4ec5097b23df80ebf7a553011a61598a916ac7c7
ishikawanagito <ishikawanagito@localhost>
2025-05-20 12:56:10 +0900

    paiza2

---
+++ src/javaPractice/paiza2.java
@@ -0,0 +1,23 @@
+package javaPractice;
+
+public class paiza2 {
+
+	public static void main(String[] args) {
+		
+		//演習1-1
+		System.out.println(4 + 6);
+		
+		//演習1-2
+		System.out.println(3 - 5);
+		
+		//演習1-3
+		System.out.println(3 * 5);
+		
+		//演習1-4
+		System.out.println(6 / 4);
+		
+		//演習1-5
+		System.out.println(5 % 3);
+	}
+
+}
