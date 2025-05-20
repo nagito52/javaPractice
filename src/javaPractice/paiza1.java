d870a70f03fdb032d14addf86d9df286949d5838
ishikawanagito <ishikawanagito@localhost>
2025-05-20 12:31:54 +0900

    paiza1

--- src/javaPractice/paiza1.java
+++ src/javaPractice/paiza1.java
@@ -3,7 +3,19 @@ package javaPractice;
 public class paiza1 {
 
 	public static void main(String[] args) {
-		//演習1
+		//演習2-1
+		System.out.println("paiza");
+		
+		//演習3-1
+		System.out.println("paiza");
+		
+		//演習3-2
+		System.out.println("paiza");
+		
+		//演習3-3
+		System.out.println("paiza");
+		
+		//演習3-4
 		System.out.println("paiza");
 	}
 
