d73144354c436392417cd8799bf6a4324eacee3d
ishikawanagito <ishikawanagito@localhost>
2025-05-20 11:50:52 +0900

    Lesson1

---
+++ src/javaPractice/Lesson1.java
@@ -0,0 +1,27 @@
+package javaPractice;
+
+public class Lesson1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		//演習1
+		System.out.println("石川凪大");
+		
+		//演習2
+		System.out.println("千葉県出身");
+		System.out.println("実家暮らし");
+		System.out.println("犬を飼っている");
+		
+		//演習3
+		int a = 0;
+		a = 5 * 6;
+		System.out.println(a);
+		
+		//演習4
+		System.out.print("プログラミング");
+		System.out.print("python");
+		System.out.print("java");
+	}
+
+}
